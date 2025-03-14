55f393332849e55f4270128afe3ae614d10ce606
Thuany Mendonca <thuany1997@hotmail.com>
2025-03-13 22:32:43 -0300

    Expressoes

---
+++ 09-Expressoes/script.js
@@ -0,0 +1,13 @@
+const grupoA = 100
+const grupoB = 300
+
+if (grupoA > grupoB) {
+  console.log('Grupo A tem mais pessoas')
+}
+
+// Ternário
+const vencedor = grupoA > grupoB ? 'Grupo A Venceu' : 'Grupo B Venceu'
+console.log(vencedor)
+
+const grupoAvenceu = grupoA > 50 && 'Grupo A Vencedor'
+console.log(grupoAvenceu)
